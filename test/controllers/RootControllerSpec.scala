e957f6a2bbdc480f9b047cd335eb687c75964e11
Luís Duarte <lduarte@luisduarte.net>
2017-10-12 00:24:36 +0100

    Add basic functional test for RootController

--- test/controllers/RootControllerSpec.scala
+++ test/controllers/RootControllerSpec.scala
@@ -1,5 +1,21 @@
 package controllers
 
-class RootControllerSpec {
+import org.scalatestplus.play._
+import org.scalatestplus.play.guice._
+import play.api.test._
+import play.api.test.Helpers._
 
+class RootControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
+
+  "RootController GET" should {
+    "render the index page from a new instance of controller" in {
+      val controller = new RootController(stubControllerComponents())
+      val root = controller.index().apply(FakeRequest(GET, "/"))
+
+      status(root) mustBe OK
+      contentType(root) mustBe Some("text/html")
+
+    }
+
+  }
 }
