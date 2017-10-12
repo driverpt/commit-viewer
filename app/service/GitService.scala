0eb1a97cd555864445f6bc24228d81148ae08557
Luís Duarte <lduarte@luisduarte.net>
2017-10-12 11:39:47 +0100

    Reworked Git parsing algorithm. It now takes Timestamp into consideration

--- app/service/GitService.scala
+++ app/service/GitService.scala
@@ -1,15 +1,25 @@
 package service
 
-import api.{Commit, Repository}
+import api.{Commit, Project, RepositoryUrl}
 import di.binders.RepositoryUrl
 import exceptions.GitException
-import models.{CommitEntity, ProjectEntity}
+import models.{CommitEntity, CommitRow, ProjectEntity, ProjectRow}
 
-import scala.reflect.io.{Directory, File}
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import scala.reflect.io.Directory
 import scala.sys.process._
 
-class GitService(project: ProjectEntity, commit: CommitEntity) {
-  def cloneRepository(url: RepositoryUrl): Option[Repository] = {
+class GitService(projectEntity: ProjectEntity, commitEntity: CommitEntity) {
+
+  def getAllProjects: Seq[Project] = {
+
+    val projects = Await.result(projectEntity.listAll(), Duration.Inf)
+
+    projects.map { row => Project(RepositoryUrl(row.projectId), None, "") }
+  }
+
+  def cloneRepository(url: RepositoryUrl): Option[Project] = {
     val tempDirectory = Directory.makeTemp()
     val result = s"git clone ${url.value} ${tempDirectory.path}/" !
 
@@ -17,25 +27,53 @@ class GitService(project: ProjectEntity, commit: CommitEntity) {
       throw new RuntimeException("Something went wrong")
     }
 
-    Some(Repository(url, tempDirectory.toFile, ""))
+    Some(Project(url, Some(tempDirectory.toFile), ""))
   }
 
-  def getCommitList(repository: Repository): Seq[Commit] = {
-    try {
-      val result = s"git --git-dir ${repository.localLocation.path}/.git log --oneline --no-decorate" !!
+  def getCommitList(repository: Project): Seq[Commit] = {
+    if (repository.localLocation.isEmpty) {
+      throw new IllegalArgumentException()
+    }
 
-      val commits = result
-        .split("\n")
-        .map {
-          _.split(" ", 2) match {
-            case Array(hash, message) => Commit(hash, message)
+    // Please don't do this in production!
+    val projectRow = Await.result(projectEntity.get(repository.url.value), Duration.Inf)
+
+    if (projectRow.isEmpty) {
+      try {
+        val result = Seq("git", "--git-dir", s"${repository.localLocation.get.path}/.git", "log", "--format=%h %at %s", "--no-decorate") !!
+        val commits = result
+          .split("\n")
+          .map {
+            _.split(" ", 3) match {
+              case Array(hash, timestamp, message) => Commit(hash, message, timestamp.toInt)
+            }
           }
+
+        val projectId = repository.url.value
+
+        // I'd rather have this saved into a variable for better readability
+        var error = Await.result(projectEntity.create(List(ProjectRow(projectId))), Duration.Inf).isEmpty
+        if (error) {
+          throw new RuntimeException
         }
 
-      commits
-    } catch {
-      // Rethrow Exception to up in case something went wrong with git
-      case e: RuntimeException => throw new GitException(e)
+        val commitRows = commits.map { commit => CommitRow(projectId, commit.uuid, commit.message, commit.timestamp) }
+
+        error = Await.result(commitEntity.create(commitRows.toList), Duration.Inf).isEmpty
+        if (error) {
+          // TODO: Create a Database Exception, i don't like Generic Exceptions
+          throw new RuntimeException
+        }
+
+        return commits
+      } catch {
+        // Rethrow Exception to up in case something went wrong with git
+        case e: RuntimeException => throw new GitException(e)
+      }
     }
+
+    // TODO: Make this fully reactive by returning Future (Not doing that to avoid Callback Hell)
+    val commits = Await.result(commitEntity.getCommitsForProject(repository.url.value.toLowerCase), Duration.Inf)
+    commits.map { row => Commit(row.commitId, row.commitMessage, row.timestamp) }
   }
 }
