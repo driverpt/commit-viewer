a14b7083600fdc5659f159f59cb2362575251ba6
Luís Duarte <lduarte@luisduarte.net>
2017-10-12 11:36:35 +0100

    Add support for Encoded URLs on RepositoryUrl class

--- app/models/api.scala
+++ app/models/api.scala
@@ -1,3 +1,6 @@
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
 import scala.reflect.io.File
 
 package object api {
@@ -6,7 +9,9 @@ package object api {
     def short: String = uuid.take(7)
   }
 
-  final case class RepositoryUrl(value: String) extends AnyVal
+  final case class RepositoryUrl(value: String) extends AnyVal {
+    def urlEncoded: String = URLEncoder.encode(value, StandardCharsets.US_ASCII.name())
+  }
 
   final case class Repository(url: RepositoryUrl, localLocation: File, name: String)
 }
